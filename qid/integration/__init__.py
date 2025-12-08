70b14b8cdca8c49f0b2600105808ad7bdd2133d8
DarekDGB <majescyk@gmail.com>
2025-12-08 13:04:20 +0000

    Create __init__.py

---
+++ qid/integration/__init__.py
@@ -0,0 +1,11 @@
+"""
+Integration helpers for DigiByte Q-ID.
+
+This package will host helper functions for integrating Q-ID with:
+- Adamantine Wallet
+- Guardian rules engine
+- Other DigiByte tools and services
+"""
+
+# Later we can export convenience functions here.
+__all__: list[str] = []
