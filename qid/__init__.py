2dd7503f446c966f1c3909391b9a2868692308c1
DarekDGB <majescyk@gmail.com>
2025-12-08 12:56:29 +0000

    Create __init__.py

---
+++ qid/__init__.py
@@ -0,0 +1,8 @@
+"""
+DigiByte Q-ID - Quantum-secure successor to Digi-ID.
+
+Core package placeholder. Real logic will be added step by step.
+"""
+
+# TODO: later we will expose high-level helper functions here.
+__all__: list[str] = []
